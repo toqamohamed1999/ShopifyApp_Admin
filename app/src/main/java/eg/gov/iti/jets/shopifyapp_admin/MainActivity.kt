c05eda5b96d391a9221d2afc2a12f2a9d19b3e76
toqamohamed1999 <toqa111999mohamed@gmail.com>
2023-06-21 09:55:59 +0300

    create multi images

--- app/src/main/java/eg/gov/iti/jets/shopifyapp_admin/MainActivity.kt
+++ app/src/main/java/eg/gov/iti/jets/shopifyapp_admin/MainActivity.kt
@@ -2,12 +2,16 @@ package eg.gov.iti.jets.shopifyapp_admin
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
@@ -28,26 +32,38 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavigation: BottomNavigationView
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var navController: NavController
-    private var navDestinationId : Int = 0
+    private var navDestinationId: Int = 0
+    private lateinit var hostFragment: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
+        hostFragment = findViewById(R.id.nav_host_fragment)
+
+        setupNavigation()
+        hideKeyBoard()
+        observeNetwork()
+        checkAuth()
+
+        if(isOnline(applicationContext)){
+            showView()
+        }else{
+            hideView()
+        }
+    }
+
+    private fun setupNavigation() {
         bottomNavigation = binding.bottomNavigationView
 
         navController =
             Navigation.findNavController(this, R.id.nav_host_fragment)
         NavigationUI.setupWithNavController(bottomNavigation, navController)
         setUpNavBottom(navController)
-
-        hideKeyBoard()
-        observeNetwork()
-        checkAuth()
     }
 
-    private fun hideKeyBoard(){
+    private fun hideKeyBoard() {
         KeyboardVisibilityEvent.setEventListener(
             this
         ) { isOpen ->
@@ -63,7 +79,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun checkNav(){
+    private fun checkNav() {
         if (navDestinationId == R.id.allProductsFragment || navDestinationId == R.id.allRulesFragment
         ) {
             bottomNavigation.visibility = View.VISIBLE
@@ -73,10 +89,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun checkAuth(){
+    private fun checkAuth() {
         sharedPreferences = applicationContext.getSharedPreferences("mypref", Context.MODE_PRIVATE)
 
-        if(!sharedPreferences.getBoolean("isLogin",false)){
+        if (!sharedPreferences.getBoolean("isLogin", false)) {
             navController.navigate(R.id.loginFragment)
         }
     }
@@ -87,12 +103,48 @@ class MainActivity : AppCompatActivity() {
         NetworkConnectivityObserver.observeNetworkConnection().onEach {
             if (it == MyNetworkStatus.Available) {
                 Log.e(TAG, "network is $it")
-                //networkTextView.visibility = View.GONE
+                showView()
             } else {
-                //networkTextView.visibility = View.VISIBLE
                 Log.e(TAG, "network is $it")
+                hideView()
             }
         }.launchIn(lifecycleScope)
+    }
+
+
+    private fun isOnline(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (connectivityManager != null) {
+            val capabilities =
+                connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            if (capabilities != null) {
+                if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                    Log.i(TAG, "NetworkCapabilities.TRANSPORT_CELLULAR")
+                    return true
+                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                    Log.i(TAG, "NetworkCapabilities.TRANSPORT_WIFI")
+                    return true
+                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                    Log.i(TAG, "NetworkCapabilities.TRANSPORT_ETHERNET")
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    private fun hideView() {
+        binding.noInternetLayout.visibility = View.VISIBLE
+        bottomNavigation.visibility = View.GONE
+        hostFragment.visibility = View.GONE
+    }
+
+    private fun showView(){
+        binding.noInternetLayout.visibility = View.GONE
+        bottomNavigation.visibility = View.VISIBLE
+        hostFragment.visibility = View.VISIBLE
+        navController.navigate(navDestinationId)
     }
 
 
